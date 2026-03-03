82ae2e0e0157ece0a480b4ceaf696ea9c32fe770
Kashif Raza <39432246+kashifraza01@users.noreply.github.com>
2026-03-04 02:54:58 +0500

    Refactor a.java by removing unnecessary whitespace

--- src/kashif/a.java
+++ src/kashif/a.java
@@ -1,24 +1,22 @@
 package kashif;
 
-public class a {
-	
-private static final String charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+public class a{
+private static final String charset="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-public static String encode(long num) {
-        StringBuilder sb = new StringBuilder();
-        while (num > 0) {
-            sb.append(charset.charAt((int)(num % 62)));
-            num /= 62;
+public static String encode(long num){
+        StringBuilder sb=new StringBuilder();
+        while(num>0){
+            sb.append(charset.charAt((int)(num%62)));
+            num/=62;
         }
         return sb.reverse().toString();
    }
 
-public static long decode(String s) {
-        long num = 0;
-        for (int i = 0; i < s.length(); i++) {
-            num = num * 62 + charset.indexOf(s.charAt(i));
+public static long decode(String s){
+        long num=0;
+        for(int i=0; i<s.length(); i++){
+            num=num*62+charset.indexOf(s.charAt(i));
         }
         return num;
    }
-
 }
