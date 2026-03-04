866eb012f2185817945931b924468a679977cddc
Kashif Raza <39432246+kashifraza01@users.noreply.github.com>
2026-03-03 21:10:05 -0800

    Refactor A.java for formatting and style consistency

--- src/kashif/A.java
+++ src/kashif/A.java
@@ -5,19 +5,12 @@ private static final String charset="0123456789abcdefghijklmnopqrstuvwxyzABCDEFG
 
 public static String encode(long num){
         StringBuilder sb=new StringBuilder();
-        while(num>0){
-            sb.append(charset.charAt((int)(num%62)));
-            num/=62;
-        }
+        while(num>0){ sb.append(charset.charAt((int)(num%62))); num/=62; }
         return sb.reverse().toString();
    }
-
 public static long decode(String s){
         long num=0;
-        for(int i=0; i<s.length(); i++){
-            num=num*62+charset.indexOf(s.charAt(i));
-        }
+        for(int i=0; i<s.length(); i++){ num=num*62+charset.indexOf(s.charAt(i)); }
         return num;
    }
 }
-
